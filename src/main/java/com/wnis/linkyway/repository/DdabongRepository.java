cf9c764369738e60dacb9fc6374b32437132a937
GounKim <kgukgu2001@gmail.com>
2022-05-14 00:48:49 +0900

    feat: Ddabong 레포지토리 추가

---
+++ src/main/java/com/wnis/linkyway/repository/DdabongRepository.java
@@ -0,0 +1,16 @@
+package com.wnis.linkyway.repository;
+
+
+import com.wnis.linkyway.entity.Ddabong;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface DdabongRepository extends JpaRepository<Ddabong, Long> {
+    @Query("select dd from Ddabong dd join fetch dd.member")
+    List<Ddabong> findAllIncludesMember();
+
+    @Query("select dd from Ddabong dd join fetch dd.tag")
+    List<Ddabong> findAllIncludesTag();
+}
