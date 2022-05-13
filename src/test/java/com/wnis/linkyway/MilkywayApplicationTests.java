e5ce925dcfa7c5ed6e44fdf6d814968e4c5e141e
GounKim <kgukgu2001@gmail.com>
2022-05-13 23:14:51 +0900

    feat: Init Project

---
+++ src/test/java/com/wnis/linkyway/MilkywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wnis.linkyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
