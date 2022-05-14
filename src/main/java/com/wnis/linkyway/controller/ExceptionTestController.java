9507b843f6b1fec96027a87d01efb37d574f818a
GounKim <kgukgu2001@gmail.com>
2022-05-14 10:59:41 +0900

    feat: 예외 응답 테스트 컨트롤러 추가

---
+++ src/main/java/com/wnis/linkyway/controller/ExceptionTestController.java
@@ -0,0 +1,37 @@
+package com.wnis.linkyway.controller;
+
+import com.wnis.linkyway.dto.card.CardRequest;
+import com.wnis.linkyway.validation.ValidationSequence;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.PositiveOrZero;
+
+@RestController
+@RequestMapping("/ex")
+public class ExceptionTestController {
+
+    @PostMapping("/cards")
+    public ResponseEntity<String> cardRequestTest(@Validated(ValidationSequence.class) @RequestBody CardRequest cardRequest) {
+        return ResponseEntity.ok(cardRequest.getContent());
+    }
+
+    @PostMapping("/numbers")
+    public ResponseEntity<Integer> numberRequestTest(@Valid @RequestBody NumberDTO numberDTO) {
+        return ResponseEntity.ok(numberDTO.getNumber());
+    }
+
+    @GetMapping("/path/{id}")
+    public ResponseEntity<Long> pathVariableTypeTest(@PathVariable Long id) {
+        return ResponseEntity.ok(id);
+    }
+
+    @Getter
+    public static class NumberDTO {
+        @PositiveOrZero
+        private int number;
+    }
+}
