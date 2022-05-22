8ee6c5fc7fa6b9c951c35c6d3cf51862e29063b1
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-05-22 22:08:56 +0900

    feat: 요청 dto 추가

---
+++ src/main/java/com/wnis/linkyway/dto/folder/SetFolderPathRequest.java
@@ -0,0 +1,19 @@
+package com.wnis.linkyway.dto.folder;
+
+import com.wnis.linkyway.validation.ValidationGroup.NotBlankGroup;
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+public class SetFolderPathRequest {
+    
+    @NotNull(message = "타겟 상위 폴더의 id를 입력해주세요",
+            groups = NotBlankGroup.class)
+    @Positive(message = "targetFolderId는 1 이상의 id를 입력하셔야 합니다.")
+    private Long targetFolderId;
+}
