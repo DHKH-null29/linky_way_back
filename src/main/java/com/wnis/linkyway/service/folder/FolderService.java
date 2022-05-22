89f5bd0df009446db176ced70627791d7b18f3bd
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-05-22 21:59:06 +0900

    feat: 폴더 서비스 추가

---
+++ src/main/java/com/wnis/linkyway/service/folder/FolderService.java
@@ -0,0 +1,22 @@
+package com.wnis.linkyway.service.folder;
+
+
+import com.wnis.linkyway.dto.Response;
+import com.wnis.linkyway.dto.folder.AddFolderRequest;
+import com.wnis.linkyway.dto.folder.FolderResponse;
+import com.wnis.linkyway.dto.folder.SetFolderNameRequest;
+import com.wnis.linkyway.dto.folder.SetFolderPathRequest;
+
+public interface FolderService {
+    
+    Response<FolderResponse> findFolder(Long folderId);
+    Response<FolderResponse> addSuperFolder(Long memberId);
+    
+    Response<FolderResponse> addFolder(AddFolderRequest addFolderRequest, Long memberId);
+    
+    Response<FolderResponse> setFolderPath(SetFolderPathRequest setFolderPathRequest, Long folderId);
+    
+    Response<FolderResponse> setFolderName(SetFolderNameRequest setFolderNameRequest, Long folderId);
+    
+    Response<FolderResponse> deleteFolder(Long folderId);
+}
