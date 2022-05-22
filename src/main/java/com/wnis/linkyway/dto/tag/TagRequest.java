f3ea2c0a33a266a0693b574688759f76bb3fe1ca
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-05-22 21:33:03 +0900

    style: 불필요한 주석 삭제 및 정렬

--- src/main/java/com/wnis/linkyway/dto/tag/TagRequest.java
+++ src/main/java/com/wnis/linkyway/dto/tag/TagRequest.java
@@ -1,26 +1,25 @@
 package com.wnis.linkyway.dto.tag;
 
 
-import com.wnis.linkyway.validation.ValidationGroup;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
-import static com.wnis.linkyway.validation.ValidationGroup.*;
+import static com.wnis.linkyway.validation.ValidationGroup.NotBlankGroup;
+import static com.wnis.linkyway.validation.ValidationGroup.PatternCheckGroup;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 public class TagRequest {
-
-    @NotBlank(message = "tagName을 입력해주세요", groups = NotBlankGroup.class)
-    @Size(max = 10, message = "10자 이하로 작성해주세요")
-    private String tagName;
-
+    
     @NotBlank(message = "소셜 공유 여부를 입력해주세요", groups = NotBlankGroup.class)
     @Pattern(regexp = "(true|false)", groups = PatternCheckGroup.class)
     String shareable;
+    @NotBlank(message = "tagName을 입력해주세요", groups = NotBlankGroup.class)
+    @Size(max = 10, message = "10자 이하로 작성해주세요")
+    private String tagName;
 }
