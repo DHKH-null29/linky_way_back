2c11bf9f2b9ce31cd9aa2ba0251b8b7c994ac9e7
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-05-24 23:24:34 +0900

    feat: 멤버 컨트롤러 추가

--- src/main/java/com/wnis/linkyway/controller/MemberController.java
+++ src/main/java/com/wnis/linkyway/controller/MemberController.java
@@ -1,36 +1,63 @@
 package com.wnis.linkyway.controller;
 
 import com.wnis.linkyway.dto.Response;
-import com.wnis.linkyway.dto.member.JoinRequest;
-import com.wnis.linkyway.dto.member.JoinResponse;
-import com.wnis.linkyway.dto.member.LoginRequest;
+import com.wnis.linkyway.dto.member.*;
+import com.wnis.linkyway.security.annotation.Authenticated;
+import com.wnis.linkyway.security.annotation.CurrentMember;
 import com.wnis.linkyway.service.MemberService;
 import com.wnis.linkyway.validation.ValidationSequence;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/members")
 public class MemberController {
-
+    
     private final MemberService memberService;
-
+    
     @PostMapping
-    public ResponseEntity<Response<Long>> join(
+    public ResponseEntity<Response> join(
             @Validated(ValidationSequence.class) @RequestBody JoinRequest joinRequest) {
-        return ResponseEntity.ok(Response.of(HttpStatus.OK, memberService.join(joinRequest), "회원가입 성공"));
+        Response<MemberResponse> response = memberService.join(joinRequest);
+        return ResponseEntity.status(response.getCode()).body(response);
     }
-
+    
     @PostMapping("/login")
     public ResponseEntity<Void> login(@RequestBody LoginRequest loginRequest) {
         return ResponseEntity.ok().build();
     }
-
+    
+    
+    @GetMapping("/email")
+    public ResponseEntity<Response> searchEmail(@RequestParam String email) {
+        Response<MemberResponse> response = memberService.searchEmail(email);
+        return ResponseEntity.status(response.getCode()).body(response);
+    }
+    
+    @GetMapping("/mypage")
+    @Authenticated
+    public ResponseEntity<Response> searchMyPage(@CurrentMember Long memberId) {
+        Response<MemberResponse> response = memberService.searchMyPage(memberId);
+        return ResponseEntity.status(response.getCode()).body(response);
+    }
+    
+    @PutMapping("/password")
+    @Authenticated
+    public ResponseEntity<Response> setPassword(
+            @Validated(ValidationSequence.class) @RequestBody PasswordRequest passwordRequest,
+            @CurrentMember Long memberId) {
+        Response<MemberResponse> response = memberService.setPassword(passwordRequest, memberId);
+        return ResponseEntity.status(response.getCode()).body(response);
+    }
+    
+    @DeleteMapping
+    @Authenticated
+    public ResponseEntity<Response> deleteMember(@CurrentMember Long memberId) {
+        Response<MemberResponse> response = memberService.deleteMember(memberId);
+        return ResponseEntity.status(response.getCode()).body(response);
+    }
+    
 }
