5a0736e86e891cc60469ee54cb7549a6e20b9e6a
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-05-24 23:20:19 +0900

    feat: Mapstruct 라이브러리 인터페이스 매핑 정의

--- src/main/java/com/wnis/linkyway/util/mapper/MemberMapper.java
+++ src/main/java/com/wnis/linkyway/util/mapper/MemberMapper.java
@@ -13,13 +13,23 @@ public interface MemberMapper {
     
     MemberMapper instance = Mappers.getMapper(MemberMapper.class);
     
-    
+    // 회원 가입
     Member joinRequestToMember(JoinRequest joinRequest);
+    
     @Mapping(source = "id", target="memberId")
     @Mapping(target = "email", ignore = true)
     @Mapping(target = "nickname", ignore = true)
     MemberResponse memberToJoinResponse(Member member);
     
+    // email 조회
     
+    @Mapping(target = "memberId", ignore = true)
+    @Mapping(target = "nickname", ignore = true)
+    MemberResponse memberToEmailResponse(Member member);
+    
+    
+    // 마이 페이지 조회
+    @Mapping(target = "memberId", ignore = true)
+    MemberResponse memberToMyPageResponse(Member member);
     
 }
