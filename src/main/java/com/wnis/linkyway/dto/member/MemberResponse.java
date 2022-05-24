043dc05cbe2b39fd2a107126fe1b9e4e0904835d
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-05-24 23:13:18 +0900

    feat: MemberResponse 통일

---
+++ src/main/java/com/wnis/linkyway/dto/member/MemberResponse.java
@@ -0,0 +1,16 @@
+package com.wnis.linkyway.dto.member;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class MemberResponse {
+    
+    private Long memberId;
+    private String email;
+    private String nickname;
+}
