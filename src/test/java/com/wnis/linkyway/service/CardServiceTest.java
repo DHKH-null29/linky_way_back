c82785cabf2a0fc94646d19b94db4db82c84651b
GounKim <kgukgu2001@gmail.com>
2022-05-25 15:53:27 +0900

    test: addCardSuccess / CardService 테스트 수정(정상작동)

--- src/test/java/com/wnis/linkyway/service/CardServiceTest.java
+++ src/test/java/com/wnis/linkyway/service/CardServiceTest.java
@@ -1,21 +1,23 @@
 package com.wnis.linkyway.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.doReturn;
+import com.wnis.linkyway.dto.card.CardRequest;
+import com.wnis.linkyway.entity.Card;
+import com.wnis.linkyway.entity.Folder;
+import com.wnis.linkyway.repository.CardRepository;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import com.wnis.linkyway.dto.card.CardRequest;
-import com.wnis.linkyway.entity.Card;
-import com.wnis.linkyway.entity.Folder;
-import com.wnis.linkyway.repository.CardRepository;
-import com.wnis.linkyway.service.CardServiceImpl;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class CardServiceTest {
@@ -27,38 +29,25 @@ public class CardServiceTest {
     private CardRepository cardRepository;
 
     private final String link = "https://github.com/DHKH-null29/linky_way_back/issues/12";
-    private final String link2 = "https://github.com/DHKH-null29/linky_way_back/issues/11";
     private final String title = "카드 조회";
     private final String content = "카드 조회 issue";
     private final boolean shareable = true;
     private final Folder folder = null;
 
-    private final CardRequest card = CardRequest.builder()
-                                                .link(link)
-                                                .title(title)
-                                                .content(content)
-                                                .shareable(shareable)
-                                                .folderId(1L)
-                                                .build();
-
-    @Test
-    @DisplayName("카드(북마크) 추가 성공")
-    public void addCardSuccess() {
-        // given
-        doReturn(card().toEntity()).when(cardRepository)
-                                   .save(Mockito.any(Card.class));
-
-        // when
-        final Long result = cardService.addCard(card());
-        System.out.println(result);
-
-        // then
-        assertThat(result).isNotNull();
+    private Card card() {
+        return Card.builder()
+                   .id(3L)
+                   .link(link)
+                   .title(title)
+                   .content(content)
+                   .shareable(shareable)
+                   .folder(folder)
+                   .build();
     }
 
-    private CardRequest card() {
+    private CardRequest cardRequest() {
         return CardRequest.builder()
-                          .link(link2)
+                          .link(link)
                           .title(title)
                           .content(content)
                           .shareable(shareable)
@@ -66,4 +55,21 @@ public class CardServiceTest {
                           .build();
     }
 
+    @Test
+    @DisplayName("카드(북마크) 추가 성공")
+    public void addCardSuccess() {
+        // given
+        Card card = card();
+
+        CardRequest cardRequest = cardRequest();
+
+        BDDMockito.given(cardRepository.save(any(Card.class))).willReturn(card);
+
+        // when
+        Long result = cardService.addCard(cardRequest);
+
+        // then
+        assertThat(result).isNotNull();
+        verify(cardRepository).save(Mockito.any(Card.class));
+    }
 }
