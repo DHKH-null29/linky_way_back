c87d2e492c4b310540b933430606b23c6158ae88
GounKim <kgukgu2001@gmail.com>
2022-05-25 15:50:06 +0900

    feat: addCard / CardServiceImpl 수정
    @Transactional 추가

--- src/main/java/com/wnis/linkyway/service/CardServiceImpl.java
+++ src/main/java/com/wnis/linkyway/service/CardServiceImpl.java
@@ -1,8 +1,12 @@
 package com.wnis.linkyway.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.wnis.linkyway.dto.card.CardRequest;
+import com.wnis.linkyway.dto.card.CardResponse;
+import com.wnis.linkyway.entity.Card;
+import com.wnis.linkyway.exception.common.ResourceNotFoundException;
 import com.wnis.linkyway.repository.CardRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -12,10 +16,10 @@ import lombok.RequiredArgsConstructor;
 public class CardServiceImpl implements CardService {
 
     private final CardRepository cardRepository;
-    
+
     @Override
+    @Transactional
     public Long addCard(CardRequest cardRequest) {
         return cardRepository.save(cardRequest.toEntity()).getId();
     }
-
 }
