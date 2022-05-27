3ccfcc663a9dd4068fbc0e926dff9bbe62251067
GounKim <kgukgu2001@gmail.com>
2022-05-28 01:23:03 +0900

    test: CardRepositoryTest / merge 후 발생한 오류 처리 + 메소드명 수정
    - db를 찾지 못하던 오류 수정
    - 메소드명을 더 직관적으로 수정함 ( addCard -> AddCardSuccess )

--- src/test/java/com/wnis/linkyway/repository/CardRepositoryTest.java
+++ src/test/java/com/wnis/linkyway/repository/CardRepositoryTest.java
@@ -5,19 +5,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import com.wnis.linkyway.entity.Card;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CardRepositoryTest {
 
     @Autowired
     private CardRepository cardRepository;
 
     @Test
-    @DisplayName("카드(북마크) 추가")
-    public void addCard() {
+    @DisplayName("카드(북마크) 추가 성공")
+    public void addCardSuccess() {
         // given
         final Card card = Card.builder()
                               .link("https://github.com/DHKH-null29/linky_way_back/issues/12")
@@ -37,7 +39,5 @@ public class CardRepositoryTest {
         assertThat(result.getContent()).isEqualTo("카드 조회 issue");
         assertThat(result.getShareable()).isEqualTo(true);
         assertThat(result.getFolder()).isNull();
-        
-        System.out.println("======" + result.getId());
     }
 }
