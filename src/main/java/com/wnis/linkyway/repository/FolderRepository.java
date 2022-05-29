c4d46461860f90c888e0997ce8ec93f827299cb3
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-05-30 07:33:21 +0900

    feat: 최상위 폴더 검색 레포지토리 추가

--- src/main/java/com/wnis/linkyway/repository/FolderRepository.java
+++ src/main/java/com/wnis/linkyway/repository/FolderRepository.java
@@ -14,6 +14,10 @@ public interface FolderRepository extends JpaRepository<Folder, Long> {
             "where f.id = :folderId")
     public Optional<Folder> findFolderById(@Param("folderId") Long folderId);
     
+    @Query("select f from Folder f left outer join fetch f.parent " +
+            "join f.member as m where m.id = :memberId and f.depth = 1")
+    public Optional<Folder> findFolderByMemberId(@Param("memberId") Long memberId);
+    
     @Modifying
     @Query(value = "insert into folder (name, depth, parent_folder_id, member_member_id) " +
             "values ('default', 1, null, :memberId)", nativeQuery = true)
