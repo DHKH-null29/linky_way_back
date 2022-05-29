bcc47a6f23a240fd0134551ef85ce4dab0b84b56
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-05-30 07:34:51 +0900

    test: 폴더 컨트롤러 테스트 NPE 버그 수정

--- src/test/java/com/wnis/linkyway/controller/folder/FolderControllerTest.java
+++ src/test/java/com/wnis/linkyway/controller/folder/FolderControllerTest.java
@@ -1,7 +1,9 @@
 package com.wnis.linkyway.controller.folder;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wnis.linkyway.dto.Response;
 import com.wnis.linkyway.dto.folder.AddFolderRequest;
+import com.wnis.linkyway.dto.folder.FolderResponse;
 import com.wnis.linkyway.dto.folder.SetFolderNameRequest;
 import com.wnis.linkyway.dto.folder.SetFolderPathRequest;
 import com.wnis.linkyway.security.testutils.WithMockMember;
@@ -16,10 +18,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -65,8 +71,12 @@ class FolderControllerTest {
                     .parentFolderId(1L)
                     .name("hello").build();
             
+            doReturn(Response.<FolderResponse>of(HttpStatus.OK, null, "성공"))
+                    .when(folderService).addFolder(any(), any());
+            
             mockMvc.perform(post("/api/folders")
-                            .contentType("application/json")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .characterEncoding("utf-8")
                             .content(objectMapper.writeValueAsString(addFolderRequest)))
                     .andExpect(status().isOk());
         }
