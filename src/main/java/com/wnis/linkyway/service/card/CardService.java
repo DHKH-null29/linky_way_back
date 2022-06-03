e985e0fb5aff670dadb5813289f3aae284c4d8ff
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-03 15:42:48 +0900

    feat: 개인 검색 서비스 파트 추가

--- src/main/java/com/wnis/linkyway/service/card/CardService.java
+++ src/main/java/com/wnis/linkyway/service/card/CardService.java
@@ -5,6 +5,8 @@ import com.wnis.linkyway.dto.card.CardRequest;
 import com.wnis.linkyway.dto.card.CardResponse;
 import com.wnis.linkyway.entity.Card;
 
+import java.util.List;
+
 public interface CardService {
 
     public AddCardResponse addCard(CardRequest cardRequest);
@@ -14,4 +16,6 @@ public interface CardService {
     public Card updateCard(Long cardId, CardRequest cardRequest);
     
     public void deleteCard(Long cardId);
+    
+    public List<CardResponse> personalSearchCardByContent(String keyword, Long memberId);
 }
