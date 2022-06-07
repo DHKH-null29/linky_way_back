5274bcbaa153384866c6bc49bba06e70b77f15e1
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-07 21:07:39 +0900

    refactor: 이제 서비스는 순수한 Dto만 반환합니다
    - 서비스의 리턴 변경에 따라 컨트롤러도 그에 맞게 변경해주었습니다
    - 기존 서비스의 응답 역할은 present layer에서
    처리하면 좋지만 우리 서비스는 복수의 응답을 고려할 필요가 없으므로 컨트롤러에서 모두 처리합니다

--- src/main/java/com/wnis/linkyway/controller/TagController.java
+++ src/main/java/com/wnis/linkyway/controller/TagController.java
@@ -2,16 +2,20 @@ package com.wnis.linkyway.controller;
 
 import com.wnis.linkyway.dto.Response;
 import com.wnis.linkyway.dto.tag.TagRequest;
+import com.wnis.linkyway.dto.tag.TagResponse;
 import com.wnis.linkyway.security.annotation.Authenticated;
 import com.wnis.linkyway.security.annotation.CurrentMember;
 import com.wnis.linkyway.service.tag.TagService;
 import com.wnis.linkyway.validation.ValidationSequence;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,8 +28,10 @@ public class TagController {
     @ApiOperation(value = "태그 조회", notes = "해당 회원이 가지고 있는 태그 리스트를 조회한다.")
     @Authenticated
     public ResponseEntity<Response> searchTags(@CurrentMember Long memberId) {
-        Response response = tagService.searchTags(memberId);
-        return ResponseEntity.ok(response);
+        List<TagResponse> response = tagService.searchTags(memberId);
+        return ResponseEntity
+                .status(200)
+                .body(Response.of(HttpStatus.OK, response, "성공적으로 태그를 조회했습니다"));
     }
     
     @PostMapping("/tags")
@@ -35,8 +41,9 @@ public class TagController {
             @ApiIgnore @CurrentMember Long memberId,
             @Validated(ValidationSequence.class) @RequestBody TagRequest tagRequest) {
         
-        Response response = tagService.addTag(tagRequest, memberId);
-        return ResponseEntity.ok(response);
+        TagResponse response = tagService.addTag(tagRequest, memberId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(Response.of(HttpStatus.OK, response, "태그가 성공적으로 생성되었습니다"));
     }
     
     @PutMapping("/tags/{tagId}")
@@ -47,8 +54,10 @@ public class TagController {
             @PathVariable(value = "tagId") Long tagId,
             @RequestBody TagRequest tagRequest) {
         
-        Response response = tagService.setTag(tagRequest, tagId);
-        return ResponseEntity.ok(response);
+        TagResponse response = tagService.setTag(tagRequest, tagId);
+        return ResponseEntity
+                .status(200)
+                .body(Response.of(HttpStatus.OK, response, "태그가 성공적으로 수정되었습니다"));
     }
     
     @DeleteMapping("/tags/{tagId}")
@@ -57,7 +66,9 @@ public class TagController {
     public ResponseEntity<Response> deleteTag(
             @PathVariable(value = "tagId") Long tagId) {
         
-        Response response = tagService.deleteTag(tagId);
-        return ResponseEntity.ok(response);
+        TagResponse response = tagService.deleteTag(tagId);
+        return ResponseEntity
+                .status(200)
+                .body(Response.of(HttpStatus.OK, response, "태그를 성공적으로 삭제했습니다"));
     }
 }
