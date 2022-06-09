dfc9031d08140b82ff9dc4fdce8d6ac3ab419012
GounKim <kgukgu2001@gmail.com>
2022-06-10 00:42:28 +0900

    feat: 카드의 태그가 없을 때 조회가 되지 않는 문제 해결

--- src/main/java/com/wnis/linkyway/repository/CardRepository.java
+++ src/main/java/com/wnis/linkyway/repository/CardRepository.java
@@ -20,17 +20,15 @@ public interface CardRepository extends JpaRepository<Card, Long> {
     @Query("select ct.card from CardTag ct join ct.card join ct.tag where ct.tag.id = :tagId")
     public List<Card> findCardsByTagId(@Param(value = "tagId") Long tagId);
 
-    @Query("select distinct c from Card c join fetch c.cardTags join c.folder "
-            + "where c.folder.member.id = :memberId and c.folder.id = :folderId")
+    @Query("select c from Card c join c.folder f where f.member.id = :memberId and f.id = :folderId")
     public List<Card> findCardsByMemberIdAndFolderId(@Param(value = "memberId") Long memberId,
             @Param(value = "folderId") Long folderId);
 
-    @Query("select distinct c from Card c join fetch c.cardTags join c.folder "
-            + "where (c.folder.parent.id = :folderId or c.folder.id = :folderId) and c.folder.member.id = :memberId")
+    @Query("select c from Card c join c.folder f "
+            + "where (f.parent.id = :folderId or f.id = :folderId) and f.member.id = :memberId")
     public List<Card> findLowLevelFoldersCardsByMemberIdAndFolderId(@Param(value = "memberId") Long memberId,
             @Param(value = "folderId") Long folderId);
 
-    @Query("select distinct c from Card c join fetch c.cardTags join c.folder "
-            + "where c.folder.member.id = :memberId")
+    @Query("select c from Card c join c.folder f where f.member.id = :memberId")
     public List<Card> findCardsByMemberId(@Param(value = "memberId") Long memberId);
 }
