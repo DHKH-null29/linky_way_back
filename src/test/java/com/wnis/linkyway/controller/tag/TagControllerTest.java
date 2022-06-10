30a01d289c856d113ecc556f1f79e09fb26783c6
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-11 05:16:00 +0900

    test: 태그 수정 컨트롤러 단위 테스트 유효성 응답 테스트 추가

--- src/test/java/com/wnis/linkyway/controller/tag/TagControllerTest.java
+++ src/test/java/com/wnis/linkyway/controller/tag/TagControllerTest.java
@@ -6,6 +6,7 @@ import com.wnis.linkyway.dto.tag.TagRequest;
 import com.wnis.linkyway.service.tag.TagService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.slf4j.Logger;
@@ -22,6 +23,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -51,32 +53,74 @@ class TagControllerTest {
                                  .build();
     }
 
-    @ParameterizedTest
-    @CsvSource(value = { "a,", ",", ",true" })
-    @DisplayName("태그이름 또는 소셜 공유 여부를 입력하지 않는 경우")
-    void shouldReturn400_WhenBlankProperty(String tagName, String shareable) throws Exception {
-        TagRequest tagRequest = TagRequest.builder()
-                                          .tagName(tagName)
-                                          .shareable(shareable)
-                                          .build();
-
-        mockMvc.perform(post("/api/tags/").contentType(MediaType.APPLICATION_JSON)
-                                          .content(objectMapper.writeValueAsString(tagRequest)))
-               .andExpect(status().is(400));
+    @Nested
+    @DisplayName("태그 추가")
+    class AddTag {
+        
+        @ParameterizedTest
+        @CsvSource(value = { "a,", ",", ",true" })
+        @DisplayName("태그이름 또는 소셜 공유 여부를 입력하지 않는 경우")
+        void shouldReturn400_WhenBlankProperty(String tagName, String shareable) throws Exception {
+            TagRequest tagRequest = TagRequest.builder()
+                                              .tagName(tagName)
+                                              .shareable(shareable)
+                                              .build();
+        
+            mockMvc.perform(post("/api/tags/").contentType(MediaType.APPLICATION_JSON)
+                                              .content(objectMapper.writeValueAsString(tagRequest)))
+                   .andExpect(status().is(400));
+        }
+    
+        @ParameterizedTest
+        @CsvSource(value = { "aaaaaaaaaaaaaaaaaaaaaaaa,true", "aa,faaaa", "aaaaaaaaaaaaaaaaaaaaaaaa,faaa" })
+        @DisplayName("패턴 또는 태그 이름 길이를 초과한 경우")
+        void shouldReturn400_WhenInvalidPattern(String tagName, String shareable) throws Exception {
+            TagRequest tagRequest = TagRequest.builder()
+                                              .tagName(tagName)
+                                              .shareable(shareable)
+                                              .build();
+        
+            mockMvc.perform(post("/api/tags/").contentType(MediaType.APPLICATION_JSON)
+                                              .content(objectMapper.writeValueAsString(tagRequest)))
+                   .andExpect(status().is(400));
+        }
     }
-
-    @ParameterizedTest
-    @CsvSource(value = { "aaaaaaaaaaaaaaaaaaaaaaaa,true", "aa,faaaa", "aaaaaaaaaaaaaaaaaaaaaaaa,faaa" })
-    @DisplayName("패턴 또는 태그 이름 길이를 초과한 경우")
-    void shouldReturn400_WhenInvalidPattern(String tagName, String shareable) throws Exception {
-        TagRequest tagRequest = TagRequest.builder()
-                                          .tagName(tagName)
-                                          .shareable(shareable)
-                                          .build();
-
-        mockMvc.perform(post("/api/tags/").contentType(MediaType.APPLICATION_JSON)
-                                          .content(objectMapper.writeValueAsString(tagRequest)))
-               .andExpect(status().is(400));
+    
+    
+    @Nested
+    @DisplayName("태그 수정")
+    class SetTag {
+        
+        @ParameterizedTest
+        @CsvSource(value = { "a,", ",", ",true" })
+        @DisplayName("태그이름 또는 소셜 공유 여부를 입력하지 않는 경우")
+        void shouldReturn400_WhenBlankProperty(String tagName, String shareable) throws Exception {
+            TagRequest tagRequest = TagRequest.builder()
+                                              .tagName(tagName)
+                                              .shareable(shareable)
+                                              .build();
+            
+            mockMvc.perform(put("/api/tags/1").contentType(MediaType.APPLICATION_JSON)
+                                              .content(objectMapper.writeValueAsString(tagRequest)))
+                   .andExpect(status().is(400));
+        }
+        
+        @ParameterizedTest
+        @CsvSource(value = { "aaaaaaaaaaaaaaaaaaaaaaaa,true", "aa,faaaa", "aaaaaaaaaaaaaaaaaaaaaaaa,faaa" })
+        @DisplayName("패턴 또는 태그 이름 길이를 초과한 경우")
+        void shouldReturn400_WhenInvalidPattern(String tagName, String shareable) throws Exception {
+            TagRequest tagRequest = TagRequest.builder()
+                                              .tagName(tagName)
+                                              .shareable(shareable)
+                                              .build();
+            
+            mockMvc.perform(put("/api/tags/1").contentType(MediaType.APPLICATION_JSON)
+                                              .content(objectMapper.writeValueAsString(tagRequest)))
+                   .andExpect(status().is(400));
+        }
     }
+    
+    
+    
 
 }
