5f1bc7911dd76e36a898e20b3782eba56df28a3d
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-13 16:44:36 +0900

    chore: initialize-test.sql 카드태그 id auto Increment 초기화 적용

--- src/main/resources/sqltest/initialize-test.sql
+++ src/main/resources/sqltest/initialize-test.sql
@@ -2,3 +2,4 @@ ALTER TABLE member auto_increment = 1;
 ALTER TABLE tag auto_increment = 1;
 ALTER TABLE card auto_increment = 1;
 ALTER TABLE folder auto_increment = 1;
+ALTER TABLE card_tag auto_increment = 1;
