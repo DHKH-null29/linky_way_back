77c4608323e47594fe7574739b9758502098201a
GounKim <kgukgu2001@gmail.com>
2022-06-14 17:00:29 +0900

    feat: 카드 복사를 위한 Response DTO 생성
    CopyPackageCardsRequest, CopyCardsRequest

---
+++ src/main/java/com/wnis/linkyway/dto/card/CopyCardsRequest.java
@@ -0,0 +1,39 @@
+package com.wnis.linkyway.dto.card;
+
+import com.wnis.linkyway.entity.Card;
+import com.wnis.linkyway.entity.Folder;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.wnis.linkyway.validation.ValidationGroup.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class CopyCardsRequest {
+
+    @NotBlank(message = "링크를 입력해주세요", groups = NotBlankGroup.class)
+    private String link;
+
+    @Size(max = 15)
+    private String title;
+    private String content;
+
+    public Card toEntity(Folder folder, boolean shareable) {
+        return Card.builder()
+                   .link(link)
+                   .title(title)
+                   .content(content)
+                   .shareable(shareable)
+                   .folder(folder)
+                   .build();
+    }
+}
