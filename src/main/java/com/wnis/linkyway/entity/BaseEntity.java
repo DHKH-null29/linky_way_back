91bab3a124448318e4bd1df71b7e52fbaffa2db3
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-14 12:07:31 +0900

    fix: 기존 BaseEntity 자동 갱신시 null 문제 수정

--- src/main/java/com/wnis/linkyway/entity/BaseEntity.java
+++ src/main/java/com/wnis/linkyway/entity/BaseEntity.java
@@ -1,8 +1,8 @@
 package com.wnis.linkyway.entity;
 
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
@@ -14,10 +14,10 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
-    @CreatedBy
+    @CreatedDate
     private LocalDateTime createdBy;
 
-    @LastModifiedBy
+    @LastModifiedDate
     private LocalDateTime modifiedBy;
 
 }
