1a824dd22acc8a8cf55fa1fae1ada1bdd397d667
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-16 16:45:50 +0900

    fix: Param -> PathVariable 로 수정

--- src/main/java/com/wnis/linkyway/controller/PackageController.java
+++ src/main/java/com/wnis/linkyway/controller/PackageController.java
@@ -6,10 +6,7 @@ import com.wnis.linkyway.service.PackageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,7 +18,7 @@ public class PackageController {
     private final PackageService packageService;
     
     @GetMapping("/social/{tagName}")
-    ResponseEntity<Response> searchSocialByTagName(@RequestParam(value = "tagName") String tagName) {
+    ResponseEntity<Response> searchSocialByTagName(@PathVariable(value = "tagName") String tagName) {
         List<PackageResponse> allPackageByTagName = packageService.findAllPackageByTagName(tagName);
         return ResponseEntity.ok(Response.of(HttpStatus.OK, allPackageByTagName, "개인 검색 성공"));
     }
