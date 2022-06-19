9a88091dc02b78bc5e83b23510d4ba8ec478c5df
pds0309 <ehd0309@gmail.com>
2022-06-20 01:00:17 +0900

    feat: Redis 설정 클래스 추가

---
+++ src/main/java/com/wnis/linkyway/config/redis/EmbeddedRedisConfiguration.java
@@ -0,0 +1,42 @@
+package com.wnis.linkyway.config.redis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.IOException;
+
+@Slf4j
+@Profile("local | test")
+@Configuration
+public class EmbeddedRedisConfiguration {
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void redisServer() throws IOException {
+        try {
+            redisServer = RedisServer.builder()
+                    .port(redisPort)
+                    .setting("maxmemory 128M")
+                    .build();
+            redisServer.start();
+        } catch (Exception e) {
+            log.error("", e);
+        }
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+
+}
