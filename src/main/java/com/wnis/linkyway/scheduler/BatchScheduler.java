af64466c806bd46531465d1deee6ccd7ab4da824
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-20 14:05:33 +0900

    feat: 스케줄링 설정

---
+++ src/main/java/com/wnis/linkyway/scheduler/BatchScheduler.java
@@ -0,0 +1,44 @@
+package com.wnis.linkyway.scheduler;
+
+import com.wnis.linkyway.config.BatchConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class BatchScheduler {
+
+    private final JobLauncher jobLauncher;
+    private final BatchConfig batchConfig;
+    
+    private final long MINUTE = 1000L * 60;
+    
+    @Scheduled(fixedDelay = 5 * MINUTE)
+    public void runJob() {
+        
+        // job Parameter 설정
+        Map<String, JobParameter> confMap = new HashMap<>();
+        confMap.put("time", new JobParameter(System.currentTimeMillis()));
+        JobParameters jobParameters = new JobParameters(confMap);
+
+        try {
+            jobLauncher.run(batchConfig.job(), jobParameters);
+        } catch (JobExecutionAlreadyRunningException
+                 | JobRestartException
+                 | JobInstanceAlreadyCompleteException
+                 | JobParametersInvalidException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
