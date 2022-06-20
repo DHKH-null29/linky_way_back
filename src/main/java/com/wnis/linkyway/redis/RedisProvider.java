e958d0804946f9391d6c09fea2c99c3d22fd9fbd
pds0309 <ehd0309@gmail.com>
2022-06-20 10:09:53 +0900

    feat: RedisProvider 클래스 추가
    redis set , get ,delete 역할

---
+++ src/main/java/com/wnis/linkyway/redis/RedisProvider.java
@@ -0,0 +1,56 @@
+package com.wnis.linkyway.redis;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RedisProvider {
+
+    private final StringRedisTemplate redisTemplate;
+    private final ObjectMapper objectMapper;
+
+    public String getData(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public <T> T getData(String key, Class<T> classType) {
+        try {
+            ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+            return objectMapper.readValue(valueOperations.get(key), classType);
+        }
+        catch (JsonProcessingException e) {
+            log.error("", e);
+            return null;
+        }
+    }
+
+    public void setDataWithExpiration(String key, String value, long expireMills) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        Duration expireDuration = Duration.ofMillis(expireMills);
+        valueOperations.set(key, value, expireDuration);
+    }
+
+    public <T> void setDataWithExpiration(String key, T data, long expireMills) {
+        try {
+            ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+            Duration expireDuration = Duration.ofMillis(expireMills);
+            valueOperations.set(key, objectMapper.writeValueAsString(data), expireDuration);
+        } catch (JsonProcessingException e) {
+            log.error("", e);
+        }
+    }
+
+    public void deleteData(String key) {
+        redisTemplate.delete(key);
+    }
+
+}
