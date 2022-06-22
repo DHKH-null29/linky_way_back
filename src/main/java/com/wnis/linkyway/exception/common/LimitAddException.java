d2be82829dca0b987d87544cc1937e7cebd20731
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-23 08:49:02 +0900

    feat: 추가 제한 비즈니스 예외 추가

---
+++ src/main/java/com/wnis/linkyway/exception/common/LimitAddException.java
@@ -0,0 +1,8 @@
+package com.wnis.linkyway.exception.common;
+
+public class LimitAddException extends ResourceConflictException{
+    
+    public LimitAddException(String message) {
+        super(message);
+    }
+}
