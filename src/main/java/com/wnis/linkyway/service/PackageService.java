0e86a0c99b28ff44ef0216d8413905802fe617be
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-23 11:35:25 +0900

    feat: 패키지 태그 기반 검색으로 변경 및 페이징 추가

--- src/main/java/com/wnis/linkyway/service/PackageService.java
+++ src/main/java/com/wnis/linkyway/service/PackageService.java
@@ -1,53 +1,41 @@
 package com.wnis.linkyway.service;
 
-import com.wnis.linkyway.dto.PackageDto;
 import com.wnis.linkyway.dto.PackageResponse;
+import com.wnis.linkyway.entity.Tag;
 import com.wnis.linkyway.repository.CardTagRepository;
+import com.wnis.linkyway.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class PackageService {
     
     private final CardTagRepository cardTagRepository;
+    private final TagRepository tagRepository;
     
-    public List<PackageResponse> findAllPackageByTagName(String tagName) {
+    public List<PackageResponse> findAllPackageByTagName(String tagName, Pageable pageable) {
         
-        List<PackageDto> packageDtoList = cardTagRepository.findAllPackageDtoByTagName(tagName);
-        Map<Long, Map<Long, List<PackageDto>>> map = packageDtoList.stream()
-                                                                   .collect(Collectors.groupingBy(
-                                                                               PackageDto::getMemberId,
-                                                                               Collectors.groupingBy(
-                                                                                       PackageDto::getTagId)));
-        
-        
-        
-        return manufacturePackageDtoMapToPackageResponse(map);
-    }
-    
-    
-    private List<PackageResponse>  manufacturePackageDtoMapToPackageResponse(Map<Long, Map<Long, List<PackageDto>>> map) {
-        List<PackageResponse> response = new ArrayList<>();
-    
-        for (Map.Entry<Long, Map<Long, List<PackageDto>>> firstEntry : map.entrySet()) {
-            for (Map.Entry<Long, List<PackageDto>> secondEntry : firstEntry.getValue().entrySet()) {
-                List<PackageDto> items = secondEntry.getValue();
-                PackageResponse packageResponse = PackageResponse.builder()
-                                                                 .memberId(firstEntry.getKey())
-                                                                 .nickname(items.get(0).getNickname())
-                                                                 .tagId(secondEntry.getKey())
-                                                                 .tagName(items.get(0).getTagName())
-                                                                 .numberOfCard(items.size())
-                                                                 .build();
-                response.add(packageResponse);
-            }
+        List<Tag> tagList = tagRepository.findAllDistinctTagListByTagName(tagName, pageable);
+        List<PackageResponse> packageResponseList = new ArrayList<>();
+        for (Tag t : tagList) {
+            long numberOfCard = cardTagRepository.countByTagId(t.getId());
+            PackageResponse packageResponse = PackageResponse.builder()
+                    .tagId(t.getId())
+                    .tagName(t.getName())
+                    .memberId(t.getMember().getId())
+                    .nickname(t.getMember().getNickname())
+                    .numberOfCard(numberOfCard)
+                    .build();
+            packageResponseList.add(packageResponse);
         }
-        return response;
+        return packageResponseList;
     }
+    
+    
+    
 }
