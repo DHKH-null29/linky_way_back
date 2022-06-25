4647f819ba388847dff38df5fed141ccec95e87f
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-25 18:15:42 +0900

    feat: isDeleted = true -> 완전히 삭제, isDeleted = false, 복원(isDeleted 속성 = false로 업데이트)

--- src/main/java/com/wnis/linkyway/controller/TrashController.java
+++ src/main/java/com/wnis/linkyway/controller/TrashController.java
@@ -24,7 +24,14 @@ public class TrashController {
     @ApiOperation(value = "삭제 & 복원", notes = "isDeleted가 true이면 삭제, isDeleted가 false이면 복원이 된다 List 형태로 card id를 받는다")
     ResponseEntity<Response<List<Long>>> updateCardIsDeletedTrueOrFalse(@RequestBody List<Long> ids, @CurrentMember Long memberId,
             @RequestParam Boolean isDeleted) {
-        List<Long> response = trashService.updateDeleteCardTrueOrFalse(ids, memberId, isDeleted);
+        List<Long> response = null;
+        
+        if (isDeleted == true) {
+            response = trashService.deleteCompletely(ids, memberId);
+        } else { //isDeleted == false
+            response = trashService.updateDeleteCardFalse(ids, memberId);
+        }
+        
         String message = isDeleted ? "삭제 성공" : "복원 성공";
         return ResponseEntity.ok(Response.of(HttpStatus.OK, response, message));
     }
