ddbb8289f5f0adf69d4e36cc8752fca4dc5009e4
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-25 18:47:24 +0900

    feat: 보안 인증 어노테이션 추가

--- src/main/java/com/wnis/linkyway/controller/TrashController.java
+++ src/main/java/com/wnis/linkyway/controller/TrashController.java
@@ -2,6 +2,7 @@ package com.wnis.linkyway.controller;
 
 import com.wnis.linkyway.dto.Response;
 import com.wnis.linkyway.dto.card.CardResponse;
+import com.wnis.linkyway.security.annotation.Authenticated;
 import com.wnis.linkyway.security.annotation.CurrentMember;
 import com.wnis.linkyway.service.TrashService;
 import io.swagger.annotations.ApiOperation;
@@ -22,6 +23,7 @@ public class TrashController {
     
     @PutMapping
     @ApiOperation(value = "삭제 & 복원", notes = "isDeleted가 true이면 삭제, isDeleted가 false이면 복원이 된다 List 형태로 card id를 받는다")
+    @Authenticated
     ResponseEntity<Response<List<Long>>> updateCardIsDeletedTrueOrFalse(@RequestBody List<Long> ids, @CurrentMember Long memberId,
             @RequestParam Boolean isDeleted) {
         List<Long> response = null;
@@ -38,6 +40,7 @@ public class TrashController {
     
     @GetMapping
     @ApiOperation(value = "삭제된 카드 조회", notes = "isDeleted가 true인 카드들을 조회한다")
+    @Authenticated
     ResponseEntity<Response<List<CardResponse>>> findAllDeletedCard(@CurrentMember Long memberId,
             @RequestParam(value = "lastCardId", required = false) Long lastCardId, Pageable pageable) {
         List<CardResponse> response = trashService.findAllDeletedCard(memberId,lastCardId, pageable);
