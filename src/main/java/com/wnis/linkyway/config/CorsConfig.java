de7f9299fc6f06dbf0af79d3234fda689602dbcf
pds0309 <ehd0309@gmail.com>
2022-06-26 09:16:17 +0900

    feat: CORS 설정 추가

--- src/main/java/com/wnis/linkyway/config/CorsConfig.java
+++ src/main/java/com/wnis/linkyway/config/CorsConfig.java
@@ -4,13 +4,15 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 public class CorsConfig {
 
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://linkyway.org"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
