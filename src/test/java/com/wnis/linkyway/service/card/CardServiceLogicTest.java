342f2a0375a6014c41d883f10251772abf30137d
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-27 08:48:57 +0900

    test: isDeleted 검증 안된 메서드에 검증 추가하면서 테스트 결과 값 변경

--- src/test/java/com/wnis/linkyway/service/card/CardServiceLogicTest.java
+++ src/test/java/com/wnis/linkyway/service/card/CardServiceLogicTest.java
@@ -212,7 +212,7 @@ public class CardServiceLogicTest {
         
         List<CardResponse> cardResponseList = cardService.findCardsByFolderId(1L, 2L, true, PageRequest.of(0, 200));
         logger.info("{}", cardResponseList.size());
-        assertThat(cardResponseList.size()).isEqualTo(5);
+        assertThat(cardResponseList.size()).isEqualTo(4);
     }
     
 }
