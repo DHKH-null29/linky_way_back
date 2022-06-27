7f23229f879985774c2916099d32afaaf30a5755
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-27 15:33:07 +0900

    style: 패키지 컨트롤러 설명 추가

--- src/main/java/com/wnis/linkyway/controller/PackageController.java
+++ src/main/java/com/wnis/linkyway/controller/PackageController.java
@@ -3,6 +3,7 @@ package com.wnis.linkyway.controller;
 import com.wnis.linkyway.dto.PackageResponse;
 import com.wnis.linkyway.dto.Response;
 import com.wnis.linkyway.service.PackageService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +17,8 @@ import java.util.List;
 public class PackageController {
 
     private final PackageService packageService;
-    
     @GetMapping("/social")
+    @ApiOperation(value = "소셜 태그 검색", notes = "태그를 기반으로 검색한다. isLike 속성에 따라 정확히 혹은 Like 검색 수행")
     ResponseEntity<Response> searchSocialByTagName(@RequestParam(value = "tagName", required = false) String tagName) {
         List<PackageResponse> allPackageByTagName = packageService.findAllPackageByTagName(tagName);
         return ResponseEntity.ok(Response.of(HttpStatus.OK, allPackageByTagName, "개인 검색 성공"));
