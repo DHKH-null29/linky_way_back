b8de3840d43db001d0768f63a7e45d3e138a8a35
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-07-21 16:39:00 +0900

    feat: QueryDsl방식을 활용한 커서 페이징

---
+++ src/main/java/com/wnis/linkyway/repository/card/CardRepositoryCustom.java
@@ -0,0 +1,115 @@
+package com.wnis.linkyway.repository.card;
+
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.wnis.linkyway.dto.card.CardDto;
+import com.wnis.linkyway.entity.QCard;
+import com.wnis.linkyway.entity.QCardTag;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CardRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+    private final QCard card = QCard.card;
+    private final QCardTag cardTag = QCardTag.cardTag;
+
+    public List<CardDto> findAllCardContainKeyword(Long lastIdx, String keyword, Long memberId,
+        Pageable pageable) {
+        return jpaQueryFactory.select(Projections.constructor(CardDto.class,
+                card.id,
+                card.link,
+                card.title,
+                card.content,
+                card.isPublic,
+                card.isDeleted,
+                card.folder.id))
+            .from(card)
+            .orderBy(card.id.desc())
+            .where(card.folder.member.id.eq(memberId)
+                .and(card.title.contains(keyword).or(card.content.contains(keyword)))
+                .and(cursorId(lastIdx)))
+            .limit(pageable.getPageSize())
+            .fetch();
+    }
+
+    private BooleanExpression cursorId(Long lastIdx) {
+        if (lastIdx == null) {
+            return null;
+        }
+        return card.id.lt(lastIdx);
+    }
+
+
+    public List<CardDto> findAllCardByTadId(Long lastIdx, Long tagId, Pageable pageable) {
+        return jpaQueryFactory.select(Projections.constructor(CardDto.class,
+                card.id,
+                card.link,
+                card.title,
+                card.content,
+                card.isPublic,
+                card.isDeleted,
+                card.folder.id))
+            .from(cardTag)
+            .orderBy(card.id.desc())
+            .join(cardTag.card, card)
+            .where(cardTag.tag.id.eq(tagId).and(cursorId(lastIdx)))
+            .limit(pageable.getPageSize())
+            .fetch();
+    }
+
+    public List<CardDto> findAllCardByMemberId(Long lastIdx, Long memberId, Pageable pageable) {
+        return jpaQueryFactory.select(Projections.constructor(CardDto.class,
+                card.id,
+                card.link,
+                card.title,
+                card.content,
+                card.isPublic,
+                card.isDeleted,
+                card.folder.id))
+            .from(card)
+            .orderBy(card.id.desc())
+            .where(card.folder.member.id.eq(memberId).and(cursorId(lastIdx)))
+            .limit(pageable.getPageSize())
+            .fetch();
+    }
+
+    public List<CardDto> findAllCardByFolderId(Long lastIdx, Long folderId, Pageable pageable) {
+        return jpaQueryFactory.select(Projections.constructor(CardDto.class,
+                card.id,
+                card.link,
+                card.title,
+                card.content,
+                card.isPublic,
+                card.isDeleted,
+                card.folder.id))
+            .from(card)
+            .orderBy(card.id.desc())
+            .where(card.folder.id.eq(folderId).and(cursorId(lastIdx)))
+            .limit(pageable.getPageSize())
+            .fetch();
+    }
+
+    public List<CardDto> findAllCardByFolderIds(Long lastIdx, List<Long> folderList,
+        Pageable pageable) {
+        return jpaQueryFactory.select(Projections.constructor(CardDto.class,
+                card.id,
+                card.link,
+                card.title,
+                card.content,
+                card.isPublic,
+                card.isDeleted,
+                card.folder.id))
+            .from(card)
+            .orderBy(card.id.desc())
+            .where(card.folder.id.in(folderList).and(cursorId(lastIdx)))
+            .limit(pageable.getPageSize())
+            .fetch();
+    }
+}
---
+++ src/main/java/com/wnis/linkyway/repository/cardtag/CardTagRepositoryCustom.java
@@ -0,0 +1,67 @@
+package com.wnis.linkyway.repository.cardtag;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.wnis.linkyway.dto.card.CardDto;
+import com.wnis.linkyway.dto.cardtag.CardTagDto;
+import com.wnis.linkyway.entity.Card;
+import com.wnis.linkyway.entity.QCard;
+import com.wnis.linkyway.entity.QCardTag;
+import com.wnis.linkyway.entity.QTag;
+import java.util.Collection;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@AllArgsConstructor
+public class CardTagRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    private final QCardTag cardTag = QCardTag.cardTag;
+    private final QCard card = QCard.card;
+    private final QTag tag = QTag.tag;
+
+    public List<CardTagDto> findCardTagByCardId(Collection<Long> ids) {
+        return jpaQueryFactory.select(
+                Projections.constructor(CardTagDto.class,
+                    card.id,
+                    card.link,
+                    card.title,
+                    card.content,
+                    card.isPublic,
+                    card.folder.id,
+                    card.isDeleted,
+                    tag.id,
+                    tag.name
+                )
+            ).from(cardTag)
+            .join(cardTag.card, card)
+            .join(cardTag.tag, tag)
+            .where(card.id.in(ids))
+            .fetch();
+    }
+
+    public List<CardTagDto> findCardTagByCard(Collection<Card> cards) {
+        return jpaQueryFactory.select(
+                Projections.constructor(CardTagDto.class,
+                    card.id,
+                    card.link,
+                    card.title,
+                    card.content,
+                    card.isPublic,
+                    card.folder.id,
+                    card.isDeleted,
+                    tag.id,
+                    tag.name
+                )
+            ).from(cardTag)
+            .join(cardTag.card, card)
+            .join(cardTag.tag, tag)
+            .where(card.in(cards))
+            .fetch();
+    }
+
+}
