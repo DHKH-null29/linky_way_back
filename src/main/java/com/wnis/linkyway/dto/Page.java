4b1dae70296cb8fbbfb1e8ed20ba20dfbea36e84
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-08-02 13:05:57 +0900

    feat: Page 객체 추가
    응답 형태를 개선하기 위함
    
    - 응답은 content, hasNext, lastIdx로 구성되어 있음.
    - 불변을 통해 Page
    데이터가 손상되는 일이 없도록 함

---
+++ src/main/java/com/wnis/linkyway/dto/Page.java
@@ -0,0 +1,32 @@
+package com.wnis.linkyway.dto;
+
+import java.util.Collections;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class Page<T> {
+
+    private final List<T> content;
+    private final Boolean hasNext;
+    private final Long lastIndex;
+
+    public static <T> Page<T> of(List<T> content, boolean hasNext, Long lastIndex) {
+        return new Page<>(content, hasNext, lastIndex);
+    }
+
+    public List<T> getContent() {
+        return Collections.unmodifiableList(this.content);
+    }
+
+    public boolean isHasNext() {
+        return hasNext;
+    }
+
+    public Long getLastIndex() {
+        return lastIndex;
+    }
+}
